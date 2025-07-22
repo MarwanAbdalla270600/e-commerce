872858650facc2e47bae650bb55eb3a35c3bb814
Marwan Abdalla <marwan3m2000@gmail.com>
2025-07-22 20:53:31 +0200

    shopping cart frame finished

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -21,7 +21,7 @@ export default function MobileSidebar() {
         <div className="absolute top-0 left-0 w-full h-screen flex flex-col items-start gap-4 bg-black py-8 px-4">
           <button
             onClick={() => setOpen(false)}
-            className="border p-2 rounded-md cursor-pointer"
+            className="border p-3 rounded-md cursor-pointer"
           >
             <X className="size-4" />
           </button>
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ShoppingBasket } from "lucide-react";
+import { ShoppingBasket, X } from "lucide-react";
 import { useState } from "react";
 import { ShoppingCart as Cart } from "lucide-react";
 export default function ShoppingCart() {
@@ -14,6 +14,25 @@ export default function ShoppingCart() {
         >
           <Cart className="size-4"></Cart>
         </button>
+      )}
+
+      {isOpen && (
+        <>
+          <button className="border p-3 rounded-md cursor-pointer">
+            <Cart className="size-4"></Cart>
+          </button>{" "}
+          <div className="absolute w-full top-0 right-0 h-screen md:w-96 bg-black py-8 px-4">
+            <div className="flex justify-between items-center">
+              <h2>My Cart</h2>
+              <button
+                onClick={() => setOpen(false)}
+                className="border p-3 rounded-md cursor-pointer"
+              >
+                <X className="size-4" />
+              </button>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
